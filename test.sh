f898add5368d0bbac12d2c4b244d52c1e1456497
Adekabang <iban.ninja@gmail.com>
2024-04-28 21:03:43 -0400

    fix test script

--- test.sh
+++ test.sh
@@ -10,15 +10,19 @@
 #from internal
 echo "check thor http (80) from tiger01"
 himage tiger01 nc -zvn 200.222.1.12 80 #thor
+echo ""
 
 echo "check dedalus ftp (21) from tiger01"
 himage tiger01 nc -zvn 200.222.1.11 21 #dedalus
+echo ""
 
 echo "check icaro http (80) from tiger01"
 himage tiger01 nc -zvn 10.0.2.3 80 #icaro
+echo ""
 
 echo "check zeus ssh (22) from tiger01"
 himage tiger01 nc -zvn 10.0.2.2 22 #zeus
+echo ""
 
 
 # err=0
