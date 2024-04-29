13ff0e94c55065ac45b00fc3a7292ccc0033facd
Adekabang <iban.ninja@gmail.com>
2024-04-28 20:51:29 -0400

    fix test script

--- test.sh
+++ test.sh
@@ -8,11 +8,28 @@
 # startCheck "$eid"
 
 #from internal
-himage tiger01 nmap -Pn -p80 200.222.1.12 #thor
-himage tiger01 nmap -Pn -p21 200.222.1.11 #dedalus
-himage tiger01 nmap -Pn -p80 10.0.2.3 #icaro
-himage tiger01 nmap -Pn -p22 10.0.2.2 #zeus
+# himage tiger01 nmap -Pn -p80 200.222.1.12 #thor
+# himage tiger01 nmap -Pn -p21 200.222.1.11 #dedalus
+# himage tiger01 nmap -Pn -p80 10.0.2.3 #icaro
+# himage tiger01 nmap -Pn -p22 10.0.2.2 #zeus
 
+check_port 22 10.0.22
+
+check_port() {
+    (echo ^]; echo quit) | timeout --signal=9 5 himage tiger01 telnet $1 $2 > /dev/null 2>&1    
+    TELNET_EXIT_CODE=$?
+        
+    if [[ $TELNET_EXIT_CODE -ne 0 ]]; then
+        nc -w $TIMEOUT -z $1 $2 > /dev/null 2>&1
+        NC_EXIT_CODE=$?
+    fi
+
+    if [[ $TELNET_EXIT_CODE -eq 0 ]] || [[ $NC_EXIT_CODE -eq 0 ]]; then
+        echo "success"
+    else
+        echo "fail"
+    fi
+}
 
 # err=0
 # # ftp
