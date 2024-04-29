218fd34d66c7ee07704c64bfb3266c975f23f9ac
Adekabang <iban.ninja@gmail.com>
2024-04-28 21:10:27 -0400

    fix test script

--- test.sh
+++ test.sh
@@ -1,45 +1,78 @@
 #!/bin/sh
 
-# . ../common/procedures.sh
+echo "Acess Testing"
+echo "========================================="
+echo "FROM TIGER01"
 
-# err=0
-
-# eid=`imunes -b services.imn | awk '/Experiment/{print $4; exit}'`
-# startCheck "$eid"
-
-#from internal
-echo "check thor http (80) from tiger01"
+echo "check thor http (80) from tiger01: should OPEN"
 himage tiger01 nc -zvn 200.222.1.12 80 #thor
 echo ""
 
-echo "check dedalus ftp (21) from tiger01"
+echo "check dedalus ftp (21) from tiger01: should OPEN"
 himage tiger01 nc -zvn 200.222.1.11 21 #dedalus
 echo ""
 
-echo "check icaro http (80) from tiger01"
+echo "check icaro http (80) from tiger01: should OPEN"
 himage tiger01 nc -zvn 10.0.2.3 80 #icaro
 echo ""
 
-echo "check zeus ssh (22) from tiger01"
+echo "check zeus ssh (22) from tiger01: should OPEN"
 himage tiger01 nc -zvn 10.0.2.2 22 #zeus
 echo ""
 
+echo "========================================="
+echo "FROM TIGER02"
 
-# err=0
-# # ftp
-# himage @$eid netstat -an | grep LISTEN | grep -q "21"
-# if [ $? -ne 0 ]; then
-#     echo "FTP error"
-#     err=1
-# fi
+echo "check thor http (80) from tiger02: should OPEN"
+himage tiger02 nc -zvn 200.222.1.12 80 #thor
+echo ""
 
-# # ssh
-# himage SSH@$eid netstat -an | grep LISTEN | grep -q "22"
-# if [ $? -ne 0 ]; then
-#     echo "SSH error"
-#     err=1
-# fi
+echo "check dedalus ftp (21) from tiger02: should OPEN"
+himage tiger02 nc -zvn 200.222.1.11 21 #dedalus
+echo ""
 
-# imunes -b -e $eid
+echo "check icaro http (80) from tiger02: should OPEN"
+himage tiger02 nc -zvn 10.0.2.3 80 #icaro
+echo ""
 
-# thereWereErrors $err
+echo "check zeus ssh (22) from tiger02: should OPEN"
+himage tiger02 nc -zvn 10.0.2.2 22 #zeus
+echo ""
+
+echo "========================================="
+echo "FROM TIGER03"
+
+echo "check thor http (80) from tiger03: should OPEN"
+himage tiger03 nc -zvn 200.222.1.12 80 #thor
+echo ""
+
+echo "check dedalus ftp (21) from tiger03: should OPEN"
+himage tiger03 nc -zvn 200.222.1.11 21 #dedalus
+echo ""
+
+echo "check icaro http (80) from tiger03: should CLOSED"
+himage tiger03 nc -zvn 10.0.2.3 80 #icaro
+echo ""
+
+echo "check zeus ssh (22) from tiger03: should OPEN"
+himage tiger03 nc -zvn 10.0.2.2 22 #zeus
+echo ""
+
+echo "========================================="
+echo "FROM ATTACKER"
+
+echo "check thor http (80) from attacker: should OPEN"
+himage attacker nc -zvn 200.222.1.12 80 #thor
+echo ""
+
+echo "check dedalus ftp (21) from attacker: should OPEN"
+himage attacker nc -zvn 200.222.1.11 21 #dedalus
+echo ""
+
+echo "check icaro http (80) from attacker: should CLOSED"
+himage attacker nc -zvn 10.0.2.3 80 #icaro
+echo ""
+
+echo "check zeus ssh (22) from attacker: should CLOSED"
+himage attacker nc -zvn 10.0.2.2 22 #zeus
+echo ""
